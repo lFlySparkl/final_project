9572304873071327ecf2353c9ae2f43a339d113e
lFlySparkl <dmitrii.barasii@gmail.com>
2023-11-14 10:29:12 +0200

    update

--- final_project/sort_folder.py
+++ final_project/sort_folder.py
@@ -172,7 +172,6 @@ def unzip_archives(path: str):
 
 def main(path):
     get_path = False
-    # path = str(input("Write path to folder: "))
 
     while get_path == False:
         if not Path(path).exists():
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(name = "book_helper",
-      version = "0.1.0",
+      version = "0.1.4",
       license='MIT',
+      packages=find_namespace_packages(),
       entry_points={'console_scripts': ['book_helper = final_project.main:main']})
